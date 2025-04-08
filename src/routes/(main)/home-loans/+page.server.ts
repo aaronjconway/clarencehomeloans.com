8dbd7e95553ebe75b85d493b0bdf033bda75915c
Aaron Conway <aaron@kaseyhomeloans.com>
2025-04-08 00:46:58 -0700

    fix env

--- src/routes/(main)/home-loans/+page.server.ts
+++ src/routes/(main)/home-loans/+page.server.ts
@@ -1,10 +1,10 @@
 import type { PageServerLoad } from './$types';
-import { DIRECTUS_ACCESS_TOKEN } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { createDirectus, staticToken, rest, readItems } from '@directus/sdk';
 
 
 const client = createDirectus('https://production-directus.8rjfpz.easypanel.host')
-	.with(staticToken(DIRECTUS_ACCESS_TOKEN))
+	.with(staticToken(env.DIRECTUS_ACCESS_TOKEN))
 	.with(rest());
 
 const withTimeout = <T>(promise: Promise<T>, ms: number): Promise<T> => {
