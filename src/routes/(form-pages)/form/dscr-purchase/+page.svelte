0a294b5798c54a90db0305db1280df2bf5920a69
Aaron Conway <aaron@kaseyhomeloans.com>
2025-04-16 06:22:59 -0700

    correct dscr pages

--- src/routes/(form-pages)/form/dscr-purchase/+page.svelte
+++ src/routes/(form-pages)/form/dscr-purchase/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import DscrRefinanceForm from './DSCRRefinanceForm.svelte'
+    import DSCRPurchaseForm from './DSCRPurchaseForm.svelte'
 </script>
 
-<DscrRefinanceForm />
+<DSCRPurchaseForm />
