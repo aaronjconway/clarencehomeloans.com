aa69ee8dac40ae0a59f6f5775e2b0ce60f235825
Aaron Conway <aaron@kaseyhomeloans.com>
2025-05-18 08:32:39 -0700

    style nit

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,41 +1,63 @@
 <script lang="ts">
-    const { href = '/' } = $props()
+	const { href = '/' } = $props();
 </script>
 
 {#if href}
-    <a {href} class="logo-wrapper">
-        <div class="logo">
-            <span class="title">Clarence Home Loans</span>
-            <span class="subtitle">Powered by Nexa Mortgage</span>
-        </div>
-    </a>
+	<a
+		{href}
+		class="logo-wrapper"
+	>
+		<div class="logo">
+			<span class="title">Clarence Home Loans</span>
+			<span class="subtitle">Powered by Nexa Mortgage</span>
+		</div>
+	</a>
 {:else}
-    <div class="logo">
-        <span class="title">Clarence Home Loans</span>
-        <span class="subtitle">Powered by Nexa Mortgage</span>
-    </div>
+	<div class="logo">
+		<span class="title">Clarence Home Loans</span>
+		<span class="subtitle">Powered by Nexa Mortgage</span>
+	</div>
 {/if}
 
 <style lang="scss">
-    a.logo-wrapper {
-        color: inherit;
-    }
-    .logo {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        white-space: nowrap;
-        font-size: var(--text-md);
-    }
+	@use '/src/styles/base';
+	a.logo-wrapper {
+		color: inherit;
+	}
+	.logo {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		white-space: nowrap;
+		font-size: var(--text);
+	}
 
-    .title {
-        //font-size: var(--text);
-        font-weight: bold;
-    }
+	.title {
+		//font-size: var(--text);
+		font-weight: bold;
+	}
 
-    .subtitle {
-        font-size: calc(var(--text-md) * 0.7);
-        line-height: 1;
-        color: var(--grey-700);
-    }
+	.subtitle {
+		font-size: calc(var(--text-md) * 0.7);
+		line-height: 1;
+		color: var(--grey-700);
+	}
+
+	@media (min-width: base.$sm) {
+		.logo {
+			font-size: var(--text);
+		}
+	}
+
+	@media (min-width: base.$md) {
+		.logo {
+			font-size: var(--text-md);
+		}
+	}
+
+	@media (min-width: base.$lg) {
+		.logo {
+			font-size: var(--text-md);
+		}
+	}
 </style>
--- src/lib/components/landing-page/Header.svelte
+++ src/lib/components/landing-page/Header.svelte
@@ -1,28 +1,49 @@
 <script>
-    import Logo from '../Logo.svelte'
+	import Logo from '../Logo.svelte';
 </script>
 
 <div class="container">
-    <div class="header">
-        <Logo href={''} />
-        <div class="contact-wrapper">
-            <a href="tel:6027372576">(602) 737-2576</a>
-            <i class="fa fa-phone"></i>
-        </div>
-    </div>
+	<div class="header">
+		<Logo href={''} />
+		<div class="contact-wrapper">
+			<a href="tel:6027372576">(602) 737-2576</a>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
-    .contact-wrapper {
-        display: flex;
-        gap: var(--space);
-        align-items: center;
-        white-space: nowrap;
-    }
-    .header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: var(--space) 0;
-    }
+	@use '/src/styles//base';
+
+	.contact-wrapper {
+		display: flex;
+		gap: var(--space);
+		align-items: center;
+		white-space: nowrap;
+	}
+
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: var(--space) 0;
+		font-size: var(--text-sm);
+	}
+
+	@media (min-width: base.$sm) {
+		.header {
+			font-size: var(--text);
+		}
+	}
+
+	@media (min-width: base.$md) {
+		.header {
+			font-size: var(--text);
+		}
+	}
+
+	@media (min-width: base.$lg) {
+		.header {
+			font-size: var(--text);
+		}
+	}
 </style>
