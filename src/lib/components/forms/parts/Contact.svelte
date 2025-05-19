bfaf41c3d0cb3c30ff5aad3b809d9fa78811b57d
Aaron Conway <aaron@kaseyhomeloans.com>
2025-05-18 17:12:11 -0700

    spelling

--- src/lib/components/forms/parts/Contact.svelte
+++ src/lib/components/forms/parts/Contact.svelte
@@ -12,7 +12,7 @@
 		<h1>Let's get you some answers.</h1>
 		<div>
 			We typically reach out same day. We don't sell or share information and
-			are looking forward to working with you regardless of the stage your in.
+			are looking forward to working with you regardless of the stage you're in.
 		</div>
 	</div>
 {/if}
