03377c5579e88b8061281278d9320c15c08cee22
Aaron Conway <aaron@kaseyhomeloans.com>
2025-05-23 06:29:14 -0700

    send formdata as json in badwords filter

--- src/routes/(main)/contact/+page.server.ts
+++ src/routes/(main)/contact/+page.server.ts
@@ -1,20 +1,18 @@
 import type { Actions } from './$types';
 //TODO:-- register system and get key
-import { env, /* FUB_SYSTEM_KEY, X_SYSTEM_KEY  */ } from '$env/dynamic/private';
+import { env /* FUB_SYSTEM_KEY, X_SYSTEM_KEY  */ } from '$env/dynamic/private';
 import { fail } from '@sveltejs/kit';
 
 // do post to followup boss
 export const actions: Actions = {
 	contact: async ({ request }) => {
-
 		const formData = await request.formData();
 		const firstName = formData.get('first_name');
 		const lastName = formData.get('last_name');
 		const phone = formData.get('phone');
 		const email = formData.get('email');
 		const message = formData.get('message') as string;
-		const special = formData.get('special')
-
+		const special = formData.get('special');
 
 		const badWords = [
 			'seo',
@@ -48,24 +46,25 @@ export const actions: Actions = {
 
 		function isFilteredMessage(message: string | null): boolean {
 			if (!message) return false;
-			return badWords.some(word => message.toLowerCase().includes(word.toLowerCase()));
+			return badWords.some((word) =>
+				message.toLowerCase().includes(word.toLowerCase())
+			);
 		}
 
 		if (isFilteredMessage(message)) {
 			await fetch('https://production-ntfy.8rjfpz.easypanel.host/chl-web', {
 				method: 'POST',
-				body: `bad message attempt: ${message}`
-			})
-			return fail(400, { message: 'spam' })
+				body: `bad message attempt: ${JSON.stringify(Object.fromEntries(formData.entries()))}`
+			});
+			return fail(400, { message: 'spam' });
 		}
-
 
 		if (special) {
 			await fetch('https://production-ntfy.8rjfpz.easypanel.host/chl-web', {
 				method: 'POST',
 				body: `special field on contact page: ${message}`
-			})
-			return fail(400, { message: 'Bot Detected' })
+			});
+			return fail(400, { message: 'Bot Detected' });
 		}
 
 		// Event data
@@ -73,7 +72,8 @@ export const actions: Actions = {
 			source: 'clarencehomeloans.com',
 			system: 'Clarence Home Loans',
 			type: 'General Inquiry',
-			description: 'An inquiry generated from the contact form on the clarencehomeloans.com/contact',
+			description:
+				'An inquiry generated from the contact form on the clarencehomeloans.com/contact',
 			pageTitle: 'Contact Us',
 			pageUrl: 'clarencehomeloans.com/contact',
 			message: message,
@@ -82,8 +82,8 @@ export const actions: Actions = {
 				firstName: firstName,
 				lastName: lastName,
 				emails: [{ value: email }],
-				phones: [{ value: phone }],
-			},
+				phones: [{ value: phone }]
+			}
 		};
 
 		const url = 'https://api.followupboss.com/v1/events';
@@ -92,11 +92,12 @@ export const actions: Actions = {
 			headers: {
 				accept: 'application/json',
 				'content-type': 'application/json',
-				authorization: 'Basic ' + Buffer.from(`${env.FUB_API_KEY}:`).toString('base64'),
+				authorization:
+					'Basic ' + Buffer.from(`${env.FUB_API_KEY}:`).toString('base64')
 				// 'SYSTEM-KEY': FUB_SYSTEM_KEY,
 				// 'X-SYSTEM-KEY': X_SYSTEM_KEY,
 			},
-			body: JSON.stringify(eventData),
+			body: JSON.stringify(eventData)
 		};
 
 		let errorMessage = '';
@@ -109,18 +110,13 @@ export const actions: Actions = {
 			});
 
 		if (errorMessage) {
-
 			await fetch('https://production-ntfy.8rjfpz.easypanel.host/chl-web', {
 				method: 'POST',
 				body: `error from contact ${errorMessage}`
-			})
-			return { success: false, message: errorMessage }
+			});
+			return { success: false, message: errorMessage };
 		}
 
-
-		return { success: true, message: '' }
+		return { success: true, message: '' };
 	}
-} satisfies Actions
-
-
-
+} satisfies Actions;
